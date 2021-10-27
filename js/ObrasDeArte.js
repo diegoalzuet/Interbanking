8d891aa020a8bd907bb38eade7c83ad7071c00e7
diegoalzuet <diegoalzuet@gmail.com>
2021-10-27 05:54:26 -0300

    Se agrego la solicitud GET HTTP de Ajax para obtener el catalogo de obras

--- js/ObrasDeArte.js
+++ js/ObrasDeArte.js
@@ -8,23 +8,24 @@ class ObraDeArte {
     // }
 
     constructor(obj){
-        this.idObra = obj.idObra;
+        this.idObra = parseInt(obj.idObra);
         this.nombre = obj.nombre;
         this.tipoObra = obj.tipoObra;
         this.tecnicaAplicada = obj.tecnicaAplicada;
         this.precio = parseFloat(obj.precio);
+        this.ruta = obj.rutaImg;
     }
     //INICIALIZA CON VALORES ALEATORIOS EL CATALOGO DE OBRAS A VENDER
     agregarAlCatalogo() {
         let div = document.getElementById('div-row');
         let contenedor = document.createElement("div");
-        contenedor.className = 'col-4';
+        contenedor.className = 'col-10 col-md-10 col-lg-6 m-auto';
         contenedor.innerHTML = `<div class="card mb-3" style="max-width: 540px;">
                                     <div class="row no-gutters">
-                                        <div class="col-md-4">
-                                            <img src="../img/arte4.jpg" class="card-img" alt="...">
+                                        <div class="col-md-5">
+                                            <img src="${this.ruta}" class="card-img" alt="...">
                                         </div>
-                                        <div class="col-md-8">
+                                        <div class="col-md-7">
                                             <div class="card-body">
                                                 <h5 class="card-title">${this.nombre}</h5>
                                                 <p class="card-text">${this.tipoObra}</p>
--- js/main.js
+++ js/main.js
@@ -1,51 +1,19 @@
-//GENERA ALEATORIAMENTE UN ARRAY DE OBRAS 
-const crearObraDeArteAleatoria = () => {
-    // console.log('Obras Generadas Aleatoriamente');
-    //let cantidadObras = 7;//parseInt(prompt('Cuantas obras de arte aleatorias quiere generar aleatoriamente en el arreglo?'));
-// const obrasDeArte = [];
-    // for (let indice = 0; indice < cantidadObras; indice++) {
-    //     obrasDeArte.push(new ObraDeArte(obrasDeArte.length, 'Obra n°' + indice, 'Tipo de Obra Generica', 'Tecnica Aplicada Generica', Math.round(10000 * Math.random())));
-    // }
+//HACE UNA PETICION GET PARA OBTENER LAS OBRAS DEL EL ARCHIVO  "ObrasDeArte.json"
+const obrasDeArte = [];
+    const URL_OBRAS_DE_ARTE = '../js/ObrasDeArte.json';
 
-    
-    const obrasDeArte = 
-    [{ idObra:0, nombre: 'Obra de arte JSON 0', tipoObra: 'Tipo de obra JSON', tecnicaAplicada: 'Tecnica Aplicada JSON', precio: 1200 },
-    { idObra: 1, nombre: 'Obra de arte JSON 1', tipoObra: 'Tipo de obra JSON', tecnicaAplicada: 'Tecnica Aplicada JSON', precio: 3200 },
-    { idObra: 2, nombre: 'Obra de arte JSON 2', tipoObra: 'Tipo de obra JSON', tecnicaAplicada: 'Tecnica Aplicada JSON', precio: 5400 },
-    { idObra: 3, nombre: 'Obra de arte JSON 3', tipoObra: 'Tipo de obra JSON', tecnicaAplicada: 'Tecnica Aplicada JSON', precio: 7600 },
-    { idObra: 4, nombre: 'Obra de arte JSON 4', tipoObra: 'Tipo de obra JSON', tecnicaAplicada: 'Tecnica Aplicada JSON', precio: 9600 },
-    { idObra: 5, nombre: 'Obra de arte JSON 5', tipoObra: 'Tipo de obra JSON', tecnicaAplicada: 'Tecnica Aplicada JSON', precio: 6200 },
-    { idObra: 6, nombre: 'Obra de arte JSON 6', tipoObra: 'Tipo de obra JSON', tecnicaAplicada: 'Tecnica Aplicada JSON', precio: 7120 },
-    { idObra: 7, nombre: 'Obra de arte JSON 7', tipoObra: 'Tipo de obra JSON', tecnicaAplicada: 'Tecnica Aplicada JSON', precio: 12900 }];
+    $.getJSON(URL_OBRAS_DE_ARTE, (respuesta, estado) => {
+        if (estado === "success") {
+            for (const obra of respuesta) {
+                obrasDeArte.push(new ObraDeArte(obra));
+                obrasDeArte[obrasDeArte.length-1].agregarAlCatalogo();
+            }
+        }
+    });
 
-    const obras =[];
-    for(const obra of obrasDeArte){
-        obras.push(new ObraDeArte(obra));
-    }
-
-    return obras;
-}
-//MUESTRA EN LA SECCION CATALO DEL HTML LAS OBRAS GENERADAS ALEATORIAMENTE 
-function cargarCatalogo(obras) {
-    for (const obra of obras) {
-        obra.agregarAlCatalogo();
-    }
-}
-//AGREGA AL CARRITO LA OBRA SELECCIONADA Y LA MUESTRA EN LA SECCION CORRESPONDIENTE
+//AGREGA AL CARRITO LA OBRA SELECCIONADA 
 function agregarAlCarrito(id) {
-    console.log(obras[id]);
-    carrito.agregarAlCarrito(obras[id]);
+    carrito.agregarAlCarrito(obrasDeArte[id]);
 }
 
-//LE ASIGNO AL SELECT EL EVENTO CHANGE PARA ORDENAR POR PRECIO EL CARRITO
-// let select = document.getElementById('selectOrdenar');
-// select.addEventListener('change', (e) => carrito.ordenar(Number(e.target.value)));
-// //LE ASIGO AL BOTON EL EVENTO CLICK PARA VACIAR EL CARRITO
-// let button = document.getElementById('vaciarCarrito');
-// button.addEventListener('click', () => carrito.vaciarCarrito());
-
-
-const obras = crearObraDeArteAleatoria();
 const carrito = new Carrito();
-//  carrito.obtenerCarritoDelStorage();
-cargarCatalogo(obras);
