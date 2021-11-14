c8291983c54d629e4a94ea3c8cf9825cd005e7b6
Diego Alzuet <diegoalzuet@gmail.com>
2021-11-14 02:43:47 -0300

    Se agregaron nuevos estilos a la pestaña 'Obra'

--- js/MiArte.js
+++ js/MiArte.js
@@ -2,33 +2,25 @@ new universalParallax().init({
     speed: 20.0
 });
 
-function cambiarFlecha() {
-    let boton = document.getElementById('btn-mas-menos');
-    if (boton.innerHTML == '<i class="fas fa-angle-double-down"></i>') {
-        boton.innerHTML = '<i class="fas fa-angle-double-up"></i>';
-    }
-    else
-        boton.innerHTML = '<i class="fas fa-angle-double-down"></i>';
-}
-
 $("#btn-mas-menos").click(() => {
     $("#mas-obras").toggle("slow", function () {
-        
-        $([document.documentElement, document.body]).animate({
-            scrollTop: $("#aca").offset().top});
 
-            let boton = document.getElementById('btn-mas-menos');
-        if (boton.innerHTML == '<i class="fas fa-angle-double-down"></i>') {
-            boton.innerHTML = '<i class="fas fa-angle-double-up"></i>';
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#aca").offset().top
+        });
+
+        let boton = document.getElementById('btn-mas-menos');
+        if (boton.innerHTML == '<i class="fas fs-1 fa-angle-double-down flecha"></i>') {
+            boton.innerHTML = '<i class="fas fs-1 fa-angle-double-up flecha"></i>';
         }
-        else{
-            boton.innerHTML = '<i class="fas fa-angle-double-down"></i>';
+        else {
+            boton.innerHTML = '<i class="fas fs-1 fa-angle-double-down flecha"></i>';
         }
     });
 }
 );
 
-$(".imagen-modal").click((e)=>{
+$(".imagen-modal").click((e) => {
     console.log(e.target.src)
     let imagen = document.getElementById("imagen-en-modal");
     imagen.src = e.target.src;
